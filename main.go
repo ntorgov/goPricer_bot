b904f5ab3a07f01ccf2c2b3ea65af64bef588116
Nick <ntorgov@gmail.com>
2023-11-26 21:38:29 +0300

    Init

---
+++ main.go
@@ -0,0 +1,104 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"strconv"
+	"strings"
+
+	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
+)
+
+func main() {
+	token := os.Getenv("TELEGRAM_BOT_TOKEN")
+
+	if token == "" {
+		log.Panic("Переменная окружения TOKEN не установлена")
+		return
+	}
+
+	bot, err := tgbotapi.NewBotAPI(token)
+	if err != nil {
+		log.Panic(err)
+	}
+
+	bot.Debug = false
+
+	log.Printf("Authorized on account %s", bot.Self.UserName)
+
+	u := tgbotapi.NewUpdate(0)
+	u.Timeout = 60
+
+	updates := bot.GetUpdatesChan(u)
+
+	for update := range updates {
+		if update.Message != nil { // If we got a message
+
+			msg := tgbotapi.NewMessage(update.Message.Chat.ID, "")
+
+			if update.Message.IsCommand() {
+				switch update.Message.Command() {
+				case "help":
+					msg.Text = "Формат ввода *Вес на ценнике*, через пробел, *Цена за этот вес*"
+				case "status":
+					msg.Text = "I'm ok."
+				default:
+					msg.Text = "I don't know that command"
+				}
+
+				bot.Send(msg)
+
+				continue
+			}
+
+			// switch update.Message.Text {
+			//case "/help":
+			//	msg := tgbotapi.NewMessage(update.Message.Chat.ID, fmt.Sprintf("Формат ввода *Вес на ценнике*, через пробел, *Цена за этот вес*"))
+			//	// msg.ReplyToMessageID = update.Message.MessageID
+			//	bot.Send(msg)
+			//	break
+			//default:
+			if !update.Message.IsCommand() {
+				command := strings.Split(update.Message.Text, " ")
+
+				if len(command) < 2 {
+					msg := tgbotapi.NewMessage(update.Message.Chat.ID, "Ошибка конвертирования")
+					msg.ReplyToMessageID = update.Message.MessageID
+					bot.Send(msg)
+					continue
+				}
+
+				priceVolume, err := strconv.ParseFloat(command[0], 64)
+				if err != nil {
+					msg := tgbotapi.NewMessage(update.Message.Chat.ID, "Не понял #1...")
+					msg.ReplyToMessageID = update.Message.MessageID
+					bot.Send(msg)
+					continue
+				}
+				// 123г = 150
+				// 1000 = x
+				priceValue, err := strconv.ParseFloat(command[1], 64)
+				if err != nil {
+					msg := tgbotapi.NewMessage(update.Message.Chat.ID, "Не понял #2...")
+					msg.ReplyToMessageID = update.Message.MessageID
+					bot.Send(msg)
+					continue
+				}
+
+				normalPrice := priceValue * 1000 / priceVolume
+
+				log.Printf("[%s] %s", update.Message.From.UserName, update.Message.Text)
+
+				msg = tgbotapi.NewMessage(update.Message.Chat.ID, fmt.Sprintf("Цена за 1000 единиц %.2f", normalPrice))
+				// msg.ReplyToMessageID = update.Message.MessageID
+
+				// break
+				// }
+			}
+
+			bot.Send(msg)
+
+		}
+	}
+}
